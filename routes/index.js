7dda058d9b56fcbc394a812646436a43ac02f9f5
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-10-07 22:40:20 -0400

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Naga' });
 });
+
+// get and render about page
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'About Me'
+  });
+});
+
+// get and render projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', {
+    title: 'Projects'
+  });
+});
+
+// get and render services page
+router.get('/services', function(req, res, next) {
+  res.render('services', {
+    title: 'Services'
+  });
+});
+
+// get and render contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {
+    title: 'Contact Me'
+  });
+});
+
 
 module.exports = router;
